64b0964a4f2e02313f03951becd9a7d46275d7b3
FlutLab.io <>
2022-05-30 03:53:19 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tesoffline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tesoffline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarNavBar(),
+    );
+  }
+}
+
+class BelajarNavBar extends StatefulWidget {
+  @override
+  _BelajarNavBarState createState() => _BelajarNavBarState();
+}
+
+class _BelajarNavBarState extends State<BelajarNavBar> {
+  int _selectedNavbar = 0;
+  var _pages = <Widget>[
+    Icon(Icons.home, size: 200, color: Colors.grey),
+    Icon(Icons.assignment, size: 200, color: Colors.grey),
+    Icon(Icons.person, size: 200, color: Colors.grey),
+  ];
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Navbar"),
+      ),
+      body: Center(child: _pages.elementAt(_selectedNavbar)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: ('Beranda'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: ('Pesanan'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: ('Profile'),
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
+      ),
+    );
+  }
+}
